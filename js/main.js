46d34c4e744728d6c9dfeb653b9629f28cae3987
Rizal Asrul Pambudi <rizal.asrul@live.com>
2016-11-15 16:41:28 +0700

    Update main.js - Add ngRoute and some config

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-var angularMiniShopApp = angular.module('angularMiniShop', []);
+var angularMiniShopApp = angular.module('angularMiniShop', ['ngRoute']);
+angularMiniShopApp.config(function($routeProvider) 
+{
+	$routeProvider
+	.when("/", 
+	{
+    	templateUrl : "product_list.htm"
+  	})
+  	.when("/cart", 
+  	{
+    	templateUrl : "cart.htm"
+  	})
+  	.when("/category", 
+  	{
+    	templateUrl : "category_list.htm"
+  	});
+});
